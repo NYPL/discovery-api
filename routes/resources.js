ef6839a4fcfbf59a3b3cfcb6dcae595d31717ff8
Paul Beaudoin <contact@paulbeaudoin.com>
2016-03-03 10:56:48 -0500

    adding resources search endpoints

--- routes/resources.js
+++ routes/resources.js
@@ -1,7 +1,5 @@
 module.exports = function(app){
 
-
-
 	app.all('*', function(req, res, next) {
 	  res.header('Access-Control-Allow-Origin', '*');
 	  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -9,64 +7,52 @@ module.exports = function(app){
 	  next();
 	});
 
+	app.get('/api/resources', function(req, res){
+
+		if (req.query.action){
+
+			if (req.query.action.toLowerCase() == 'search'){
+
+				app.resources.findById(req.query.value, function(resource){    	
+					res.type('application/json')
+						res.status(200).send(JSON.stringify(resource, null, 2))
+						return true
+					})
+
+			} else if (req.query.action.toLowerCase() == 'searchbytitle'){
+				app.resources.searchByTitle(req.query.value, function(results) {    	
+					res.type('application/json')
+					res.status(200).send(JSON.stringify(results, null, 2))
+					return true
+				})
+
+			} else if (req.query.action.toLowerCase() == 'overview'){
+
+				app.resources.overview(req.query.value, function(resource){    	
+					res.type('application/json')
+					res.status(200).send(JSON.stringify(resource, null, 2))
+					return true
+				})
 
 
-    app.get('/api/resources', function(req, res){
+			} else if (req.query.action.toLowerCase() == 'byterm'){
 
-
-
-    	if (req.query.action){
-
-    		if (req.query.action.toLowerCase() == 'search'){
-
-
-					app.resources.findById(req.query.value, function(resource){    	
-						res.type('application/json')
-			    		res.status(200).send(JSON.stringify(resource, null, 2))
-			    		return true
-			    	})
-
-			}
-
-    		if (req.query.action.toLowerCase() == 'overview'){
-    				
-
-					app.resources.overview(req.query.value, function(resource){    	
-						res.type('application/json')
-			    		res.status(200).send(JSON.stringify(resource, null, 2))
-			    		return true
-			    	})
-
-			}
-
-    		if (req.query.action.toLowerCase() == 'byterm'){
-    				
-
-					app.resources.byTerm(req.query.value, function(resource){    	
-						res.type('application/json')
-			    		res.status(200).send(JSON.stringify(resource, null, 2))
-			    		return true
-			    	})
+				app.resources.byTerm(req.query.value, function(resource){    	
+					res.type('application/json')
+						res.status(200).send(JSON.stringify(resource, null, 2))
+						return true
+					})
 
 			}
-
-
-
-
 
 		}else{
 
 			res.type('application/json')
 			res.status(500).send(JSON.stringify({error: "No Action requested"}, null, 2))
-
-
-
+			return true
 		}
 
-
-
-
-    })
+	})
 
     //other routes..
 }
