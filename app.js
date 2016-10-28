3951a9af0c732b6cd7c50b0116f51a90cef0566e
Paul Beaudoin <contact@paulbeaudoin.com>
2016-10-28 13:08:03 -0400

    initial work serializing new disc props and adherence to v0.2 api

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var cluster = require('cluster')
-var path = require('path')
+// var path = require('path')
+//
+const log = require('loglevel')
+log.setLevel('trace')
 
 if (cluster.isMaster) {
   // var numCPUs = require('os').cpus().length
@@ -13,18 +16,16 @@ if (cluster.isMaster) {
     cluster.fork()
   })
 } else {
-  // var JSONAPISerializer = require('jsonapi-serializer')
   var config = require('config')
 
   var express = require('express')
-  // var serveStatic = require('serve-static')
   var elasticsearch = require('elasticsearch')
   var pjson = require('./package.json')
 
-  var db = require(path.join(__dirname, '/lib/db.js'))
+  // var db = require(path.join(__dirname, '/lib/db.js'))
   var app = express()
 
-  app.db = db
+  // app.db = db
 
   app.thesaurus = config.thesaurus
 
@@ -37,23 +38,11 @@ if (cluster.isMaster) {
 
   require('./routes/misc')(app)
 
-  db.databaseConnectTripleStore()
+  // db.databaseConnectTripleStore()
 
   app.esClient = new elasticsearch.Client({
     host: config['Elasticsearch'].host
   })
-
-  // app.use(serveStatic(__dirname + '/public', {'index': ['index.html']} ))
-
-  // //serializer
-  // var sampleSerializer = function(sample) {
-  // 	this.serialize = function () {
-  // 		return new JSONAPISerializer('sample', sample, {
-  // 	 		attributes: ['title', 'publishYear'],
-  // 		})
-  // 	}
-
-  // }
 
   app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
@@ -65,206 +54,6 @@ if (cluster.isMaster) {
   app.get('/', function (req, res) {
     res.send(pjson.version)
   })
-
-  // app.get('/api/lccrange/:range', function(req, res) {
-  // 	db.sampleByLccRange(req.params.range, function(err,results){
-
-  // 		if (err){
-  // 			res.status(500).send( { data : [] } )
-  // 		}else{
-
-  // 			if (results.length===0){
-  // 				res.status(200).send( { data : [] } )
-  // 			}else{
-  // 				var json = new sampleSerializer(results).serialize()
-  // 				res.status(200).send(json)
-  // 			}
-  // 		}
-
-  // 	})
-  // })
-
-  // app.get('/api/classmark/:classmark', function(req, res) {
-
-  // 	db.sampleByClassmark(req.params.classmark, function(err,results){
-  // 		res.type('application/json')
-
-  // 		if (err){
-  // 			res.status(500).send( { data : [] } )
-  // 		}else{
-
-  // 			if (results.length===0){
-  // 				res.status(200).send( { data : [] } )
-  // 			}else{
-  // 				var json = new sampleSerializer(results).serialize()
-  // 				res.status(200).send(json)
-  // 			}
-  // 		}
-
-  // 	})
-  // })
-
-  // app.get('/api/bnumber/:bnumber', function(req, res) {
-
-  // 	db.returnBnumber(req.params.bnumber, function(err,results){
-  // 		res.type('application/json')
-
-  // 		if (err){
-  // 			res.status(500).send( { } )
-  // 		}else{
-
-  // 			if (results.length===0){
-  // 				res.status(200).send( { } )
-  // 			}else{
-  // 				//var json = new sampleSerializer(results).serialize()
-  // 				res.status(200).send(JSON.stringify(results[0],null,2))
-  // 			}
-  // 		}
-
-  // 	})
-  // })
-
-  // app.get('/api/items/:bnumber', function(req, res) {
-
-  // 	db.returnItemByBibIds(req.params.bnumber, function(err,results){
-  // 		res.type('application/json')
-
-  // 		if (err){
-  // 			res.status(500).send( { } )
-  // 		}else{
-
-  // 			if (results.length===0){
-  // 				res.status(200).send( { } )
-  // 			}else{
-  // 				//var json = new sampleSerializer(results).serialize()
-  // 				res.status(200).send(JSON.stringify(results, null, 2))
-  // 			}
-  // 		}
-
-  // 	})
-  // })
-
-  // app.get('/api/stocknumber/:stocknumber', function(req, res) {
-
-  // 	db.returnStockNumber(req.params.stocknumber, function(err,results){
-  // 		res.type('application/json')
-
-  // 		if (err){
-  // 			res.status(500).send( [])
-  // 		}else{
-
-  // 			if (results.length===0){
-  // 				res.status(200).send( [] )
-  // 			}else{
-  // 				//var json = new sampleSerializer(results).serialize()
-  // 				res.status(200).send(JSON.stringify(results))
-  // 			}
-  // 		}
-
-  // 	})
-  // })
-
-  // app.get('/api/owi/:owi', function(req, res) {
-
-  // 	db.returnOwi(req.params.owi, function(err,results){
-  // 		res.type('application/json')
-
-  // 		if (err){
-  // 			res.status(500).send( [])
-  // 		}else{
-
-  // 			if (results.length===0){
-  // 				res.status(200).send( [] )
-  // 			}else{
-  // 				//var json = new sampleSerializer(results).serialize()
-  // 				res.status(200).send(JSON.stringify(results))
-  // 			}
-  // 		}
-
-  // 	})
-  // })
-
-  // app.get('/api/isbnworks/:isbn', function(req, res) {
-
-  // 	db.returnWorksByIsbn(req.params.isbn, function(err,results){
-  // 		res.type('application/json')
-
-  // 		if (err){
-  // 			res.status(500).send( [])
-  // 		}else{
-
-  // 			if (results.length===0){
-  // 				res.status(200).send( [] )
-  // 			}else{
-  // 				//var json = new sampleSerializer(results).serialize()
-  // 				res.status(200).send(JSON.stringify(results))
-  // 			}
-  // 		}
-
-  // 	})
-  // })
-
-  // app.get('/api/audience/', function(req, res) {
-
-  // 	var isbn = (req.query.isbn) ? req.query.isbn : false
-  // 	var oclc = (req.query.oclc) ? req.query.oclc : false
-  // 	var bnumber = (req.query.bnumber) ? req.query.bnumber : false
-  // 	var owi = (req.query.owi) ? req.query.owi : false
-  // 	var stock = (req.query.stocknumber) ? req.query.stocknumber : false
-
-  // 	var q = []
-
-  // 	if (isbn) q.push({ 'sc:isbn' : isbn})
-
-  // 	if (oclc) {
-  // 		q.push({ 'sc:oclc' : oclc})
-  // 		q.push({ 'classify:oclc' : oclc})
-  // 	}
-  // 	if (bnumber)  q.push({ '_id' : bnumber})
-  // 	if (owi)  q.push({ 'classify:owi' : owi})
-  // 	if (stock)  q.push({ 'sc:stockNumber' : stock})
-
-  // 	if (q.length>0){
-  // 		db.returnAudience(q, function(err,results){
-  // 			res.type('application/json')
-
-  // 			if (err){
-  // 				res.status(500).send({})
-  // 			}else{
-
-  // 				if (results.length===0){
-  // 					res.status(200).send( {} )
-  // 				}else{
-  // 					//var json = new sampleSerializer(results).serialize()
-  // 					res.status(200).send(JSON.stringify(results))
-  // 				}
-  // 			}
-  // 		})
-  // 	}else{
-  // 		res.status(200).send({})
-  // 	}
-  // })
-
-  // app.get('/api/bots', function(req, res) {
-
-  // 	db.returnBots(function(err,results){
-
-  // 		res.type('application/json')
-
-  // 		if (err){
-  // 			res.status(500).send( [] )
-  // 		}else{
-
-  // 			if (results.length===0){
-  // 				res.status(200).send( [] )
-  // 			}else{
-  // 				//var json = new sampleSerializer(results).serialize()
-  // 				res.status(200).send(JSON.stringify(results))
-  // 			}
-  // 		}
-
-  // 	})
-  // })
 
   app.listen(config['Port'], function () {
     console.log('Server started on port ' + config['Port'])
--- routes/resources.js
+++ routes/resources.js
@@ -1,66 +1,73 @@
 var gatherParams = require('../lib/util').gatherParams
 
+const config = require('config')
+
 module.exports = function (app) {
   app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type')
     next()
   })
 
-  var standardParams = ['page', 'per_page', 'value', 'q', 'expandContext']
+  var standardParams = ['page', 'per_page', 'q', 'expandContext', 'ext', 'field']
 
-  var actionHandlers = {
-    // Redundant if we use resources/:id path above:
-    'lookup': { handler: app.resources.findById },
+  const VER = config.get('major_version')
 
-    'searchbytitle': { handler: app.resources.searchByTitle },
-    'search': { handler: app.resources.search, params: standardParams.concat(['filters']) },
-    'aggregations': { handler: app.resources.searchAggregations, params: standardParams.concat(['filters', 'fields']) },
-    'overview': { handler: app.resources.overview },
-    'ntriples': { handler: app.resources.overviewNtriples, contentType: 'text/plain' },
-    'jsonld': { handler: app.resources.overviewJsonld },
-    'byterm': { handler: app.resources.byTerm },
-    'searchold': { handler: app.resources.findByOldId },
-    'byowi': { handler: app.resources.findByOwi },
-    'random': { handler: (v, cb) => app.resources.randomResources(v, cb) }
+  const respond = (res, _resp, params) => {
+    var contentType = 'application/ld+json'
+    if (params.ext === 'ntriples') contentType = 'text/plain'
+
+    var resp = _resp
+    if (contentType !== 'text/plain') resp = JSON.stringify(_resp, null, 2)
+
+    res.type(contentType)
+    res.status(200).send(resp)
+    return true
   }
 
-  app.get('/api/v1/resources/:id', function (req, res) {
-    var params = gatherParams(req, actionHandlers['lookup'].params || standardParams)
-    params.id = req.params.id
-    app.resources.findById(params, function (_resp) {
-      res.type('application/ld+json')
-      res.status(200).send(JSON.stringify(_resp, null, 2))
-      return true
-    })
+  const handleError = (res, error, params) => {
+    console.error('Resources#handleError:', error)
+    res.status(500).send({ error: error.message ? error.message : error })
+    return false
+  }
+
+  app.get(`/api/v${VER}/resources$`, function (req, res) {
+    var params = gatherParams(req, standardParams)
+
+    return app.resources.search(params)
+      .then((resp) => respond(res, resp, params))
+      .catch((error) => handleError(res, error, params))
   })
 
-  app.get('/api/v1/resources', function (req, res) {
-    if (req.query.action) {
-      var action = req.query.action.toLowerCase()
+  app.get(`/api/v${VER}/resources/aggregations`, function (req, res) {
+    var params = req.params
 
-      // Error if action invalid:
-      if (Object.keys(actionHandlers).indexOf(action) < 0) {
-        res.type('application/json')
-        res.status(500).send(JSON.stringify({error: 'Invalid Action'}, null, 2))
-      } else {
-        var handlerConfig = null
-        if ((handlerConfig = actionHandlers[action])) {
-          var params = gatherParams(req, handlerConfig.params)
+    return app.resources.aggregations(params)
+      .then((resp) => respond(res, resp, params))
+      .catch((error) => handleError(res, error, params))
+  })
 
-          handlerConfig.handler(params, function (_resp) {
-            res.type(handlerConfig.contentType ? handlerConfig.contentType : 'application/ld+json')
-            res.status(200).send(JSON.stringify(_resp, null, 2))
-            return true
-          })
-        }
-      }
+  app.get(`/api/v${VER}/resources/aggregation/:field`, function (req, res) {
+    var params = req.params
 
-    // Error if no action given:
-    } else {
-      res.type('application/json')
-      res.status(500).send(JSON.stringify({error: 'No Action requested'}, null, 2))
+    return app.resources.aggregation(params)
+      .then((resp) => respond(res, resp, params))
+      .catch((error) => handleError(res, error, params))
+  })
+
+  app.get(`/api/v${VER}/resources/:uri\.:ext?`, function (req, res) {
+    var params = { uri: req.params.uri }
+
+    // var handler = app.resources.findById
+    var handler = app.resources.findByUri
+
+    if (req.params.ext === 'ntriples') {
+      handler = app.resources.overviewNtriples
     }
+
+    return handler(params)
+      .then((resp) => respond(res, resp, params))
+      .catch((error) => handleError(res, error, params))
   })
 }
