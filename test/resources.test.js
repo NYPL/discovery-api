2b1f28ece850df1f5b574d3125b2bb7e8b32ace4
Paul Beaudoin <contact@paulbeaudoin.com>
2017-01-25 16:49:11 -0500

    remove deprecated tests, add new resources tests, ensure standard js style

--- test/resources.test.js
+++ test/resources.test.js
@@ -5,24 +5,16 @@ var assert = require('assert')
 var base_url = (process.env.API_ADDRESS ? process.env.API_ADDRESS : 'http://localhost:3000')
 
 describe('Test Resources responses', function () {
-  var sampleResources = [{uri: 101164830, type: 'nypl:Component'}, {uri: 100037340, type: 'nypl:Item'}]
-  // var sampleResources = [{uri: 101164830, type: 'resourcetypes:col'}, {uri: 100037340, type: 'resourcetypes:txt'}]
+  var sampleResources = [{id: 'b10015541', type: 'nypl:Item'}, {id: 'b10022950', type: 'nypl:Item'}]
 
   this.timeout(10000)
 
   describe('GET sample resources', function () {
     sampleResources.forEach(function (spec) {
-      it(`Resource ${spec.uri} returns status code 200`, function (done) {
-        request.get(`${base_url}/api/v1/resources/${spec.uri}`, function (err, response, body) {
+      it(`Resource ${spec.id} has correct type ${spec.type}`, function (done) {
+        request.get(`${base_url}/api/v1/resources/${spec.id}`, function (err, response, body) {
           if (err) throw err
           assert.equal(200, response.statusCode)
-          done()
-        })
-      })
-
-      it(`Resource ${spec.uri} has correct type ${spec.type}`, function (done) {
-        request.get(`${base_url}/api/v1/resources/${spec.uri}`, function (err, response, body) {
-          if (err) throw err
           var doc = JSON.parse(body)
           assert(doc['@type'].indexOf(spec.type) >= 0)
           done()
@@ -31,45 +23,56 @@ describe('Test Resources responses', function () {
     })
   })
 
-  describe('GET resources random', function () {
-    var randomUrl = `${base_url}/api/v1/resources?action=random`
-    it('Resource random has requested page size', function (done) {
-      request.get(`${randomUrl}&per_page=13`, function (err, response, body) {
-        if (err) throw err
-        var doc = JSON.parse(body)
-        assert.equal(13, doc.itemListElement.length)
-        done()
-      })
-    })
-  })
-
   describe('GET resources fields', function () {
-    it('Resource data for 136050732 are what we expect', function (done) {
-      request.get(`${base_url}/api/v1/resources/136050732`, function (err, response, body) {
+    it('Resource data for b10015541 are what we expect', function (done) {
+      request.get(`${base_url}/api/v1/resources/b10022950`, function (err, response, body) {
         if (err) throw err
 
         assert.equal(200, response.statusCode)
 
         var doc = JSON.parse(body)
 
-        assert(doc.idLccCoarse[0], 'PN6157-6222')
-        assert(doc.language[0].prefLabel, 'English')
-        assert(doc.type[0].prefLabel === 'Audio')
-        assert(doc.type[0]['@id'] === 'resourcetypes:aud')
+        assert(doc.title)
+        assert.equal(doc.title[0], 'Religion--love or hate? /')
 
-        // Comic Relief (Fund raising enterprise) is a contrib:
-        assert(doc.contributor.length > 10)
-        assert(doc.contributor.filter((contrib) => contrib['@id'] === 'agents:10473263').length === 1)
+        assert(doc.contributor)
+        assert.equal(doc.contributor.length, 1)
+        assert.equal(doc.contributor[0], 'Kirshenbaum, D. (David), 1902-')
 
-        // George Carlin is a contrib:
-        assert(doc.contributor.filter((contrib) => contrib['@id'] === 'agents:11230376').length === 1)
+        assert(doc.materialType)
+        assert.equal(doc.materialType[0]['@id'], 'resourcetypes:txt')
+        assert.equal(doc.materialType[0].prefLabel, 'Text')
 
-        // Billy Crystal is a perf:
-        assert(doc['roles:prf'].length > 10)
-        assert(doc['roles:prf'].filter((perf) => perf.prefLabel.indexOf('Crystal, Billy') >= 0).length === 1)
+        assert(doc.issuance)
+        assert.equal(doc.issuance[0]['@id'], 'urn:biblevel:m')
 
-        assert(doc.subject.length >= 2)
-        assert(doc.subject.filter((subj) => subj.prefLabel.indexOf('Wit and humor') >= 0).length === 1)
+        assert(doc.dimensions)
+        assert.equal(doc.dimensions[0], '24 cm.')
+
+        assert.equal(doc.createdYear, 1974)
+
+        done()
+      })
+    })
+  })
+
+  describe('GET resources fields', function () {
+    it('Resource data for b10022734 are what we expect', function (done) {
+      request.get(`${base_url}/api/v1/resources/b10022734`, function (err, response, body) {
+        if (err) throw err
+
+        assert.equal(200, response.statusCode)
+
+        var doc = JSON.parse(body)
+
+        assert(doc.title)
+        assert.equal(doc.title[0], 'When Harlem was in vogue /')
+
+        assert.equal(doc.createdYear, 1981)
+
+        assert(doc.items)
+        assert.equal(doc.items.length, 3)
+        assert.equal(doc.items.filter((i) => i.shelfMark[0] === 'Sc E 96-780').length, 1)
 
         done()
       })
@@ -77,30 +80,27 @@ describe('Test Resources responses', function () {
   })
 
   describe('GET resources search', function () {
-    var searchAllUrl = `${base_url}/api/v1/resources?action=search`
+    var searchAllUrl = `${base_url}/api/v1/resources?q=`
 
     it('Resource search all returns status code 200', function (done) {
       request.get(searchAllUrl, function (err, response, body) {
         if (err) throw err
+
         assert.equal(200, response.statusCode)
+
         done()
       })
     })
 
-    it(`Resource search all (${searchAllUrl}) returns > 10mil results`, function (done) {
+    it(`Resource search all (${searchAllUrl}) returns lots o\' results`, function (done) {
       request.get(searchAllUrl, function (err, response, body) {
         if (err) throw err
-        var doc = JSON.parse(body)
-        assert(doc.totalResults > 10000000)
-        done()
-      })
-    })
 
-    it('Resource search all page 1 has default page size', function (done) {
-      request.get(searchAllUrl, function (err, response, body) {
-        if (err) throw err
         var doc = JSON.parse(body)
+
+        assert(doc.totalResults > 400000)
         assert.equal(50, doc.itemListElement.length)
+
         done()
       })
     })
@@ -109,7 +109,9 @@ describe('Test Resources responses', function () {
       request.get(`${searchAllUrl}&per_page=42`, function (err, response, body) {
         if (err) throw err
         var doc = JSON.parse(body)
+
         assert.equal(42, doc.itemListElement.length)
+
         done()
       })
     })
@@ -130,6 +132,9 @@ describe('Test Resources responses', function () {
       })
     })
 
+    /*
+     *  Deprecating this for now
+     *
     it('Resource search filter on agents', function (done) {
       // Filter on these agents:
       var agents = ['agents:10955903', 'agents:10334529']
@@ -154,9 +159,10 @@ describe('Test Resources responses', function () {
         })
       })
     })
+    */
 
     var dates = [1984, 1985]
-    var initialUrl = `${searchAllUrl}&filters[date]=${dates[0]}`
+    var initialUrl = `${searchAllUrl}&filters=date:${dates[0]}`
     // First just filter on the first date (objects whose start/end date range include 1984)
     request.get(initialUrl, function (err, response, body) {
       if (err) throw err
@@ -180,17 +186,27 @@ describe('Test Resources responses', function () {
         prevTotal = doc.totalResults
 
         // Now filter on both dates (adding objects whose date range includes 1985)
-        var yearFilter = dates.map((a) => `filters[date][]=${a}`).join('&')
-        request.get(`${searchAllUrl}&${yearFilter}`, function (err, response, body) {
+        // var yearFilter = dates.map((a) => `filters[date][]=${a}`).join('&')
+        request.get(`${searchAllUrl}&filters=date:[${dates.join(' TO ')}}`, function (err, response, body) {
           if (err) throw err
           var doc = JSON.parse(body)
           assert(doc.totalResults > prevTotal)
 
-          done()
+          // Now add language filter:
+          request.get(`${searchAllUrl}&filters=date:[${dates.join(' TO ')}} language.id:\"lang:kan\"`, function (err, response, body) {
+            if (err) throw err
+            var doc = JSON.parse(body)
+            assert(doc.totalResults > prevTotal)
+
+            done()
+          })
         })
       })
     })
 
+    /*
+     *  Deprecating this for now
+     *
     var parents = [101669044]
     parents.forEach((parentId) => {
       var url = `${searchAllUrl}&filters[parent]=${parentId}`
@@ -205,24 +221,6 @@ describe('Test Resources responses', function () {
             var rootParent = firstItem.memberOf[firstItem.memberOf.length - 1]
             assert(rootParent['@id'] === `res:${parentId}`)
           }
-
-          done()
-        })
-      })
-    })
-
-    /*
-    var map = {
-      'romeo & juliet': 10012622
-    }
-    Object.keys(map).forEach((q) => {
-      it(`Agent search relevance: ${q} => agents:${map[q]}`, function (done) {
-        request.get(`${searchAllUrl}&q=${q}`, function (err, response, body) {
-          if (err) throw err
-          var doc = JSON.parse(body)
-
-          var expectedAgentUri = `agents:${map[q]}`
-          assert(doc.itemListElement[0].result['@id'] === expectedAgentUri)
 
           done()
         })
