62d94f594d762d94ea1b909d1d88e2861f9d089d
Edwin Guzman <edwin.gzmn@gmail.com>
2017-04-21 15:56:50 -0400

    Checking is AWS env var is available before starting app.

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-const config = require('config');
-const log = require('loglevel');
-const swaggerDocs = require('./swagger.v0.2.json');
-const pjson = require('./package.json');
+const config = require('config')
+const log = require('loglevel')
+const swaggerDocs = require('./swagger.v0.2.json')
+const pjson = require('./package.json')
 
-require('dotenv').config();
+require('dotenv').config()
 
-var express = require('express');
-var elasticsearch = require('elasticsearch');
+var express = require('express')
+var elasticsearch = require('elasticsearch')
 
-var app = express();
+var app = express()
 
-app.thesaurus = config.thesaurus;
+app.thesaurus = config.thesaurus
 
-require('./lib/agents')(app);
-require('./lib/resources')(app);
+require('./lib/agents')(app)
+require('./lib/resources')(app)
 
 // routes
-require('./routes/agents')(app);
-require('./routes/resources')(app);
-require('./routes/misc')(app);
+require('./routes/agents')(app)
+require('./routes/resources')(app)
+require('./routes/misc')(app)
 
 app.esClient = new elasticsearch.Client({
   host: config['elasticsearch'].host
-});
+})
 
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 app.get('/', function (req, res) {
   res.send(pjson.version)
-});
+})
 
 // Just testing route
 app.get('/api/v0.1/discovery', function (req, res) {
   res.send(pjson.version)
-});
+})
 
 app.get('/api/v0.1/discovery/swagger', function (req, res) {
-  res.send(swaggerDocs);
-});
+  res.send(swaggerDocs)
+})
 
-// Could be removed for the Lambda but necessary for locally running the app.
-require('./lib/globals')(app).then((app) => {
-  app.listen(config['port'], function () {
-    console.log('Server started on port ' + config['port']);
-  });
-});
+// Only start the Express server locally:
+if (process.env.AWS_LAMBDA_FUNCTION_NAME) {
+  require('./lib/globals')(app).then((app) => {
+    app.listen(config['port'], function () {
+      console.log('Server started on port ' + config['port'])
+    })
+  })
+}
 
-module.exports = app;
+module.exports = app
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
-const server = awsServerlessExpress.createServer(app);
+const awsServerlessExpress = require('aws-serverless-express')
+const app = require('./app')
+const server = awsServerlessExpress.createServer(app)
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
