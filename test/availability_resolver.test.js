f196a60f4874a9214864adab5abfeddfe4dcabfa
Jobin Thomas <jobinthomas@nypl.org>
2017-06-16 11:14:28 -0400

    First pass on unit test for item availability status

---
+++ test/availability_resolver.test.js
@@ -0,0 +1,29 @@
+var AvailabilityResolver = require('../lib/availability_resolver.js')
+var expect = require("chai").expect;
+
+describe('Response with updated availability', function () {
+    it('includes the latest availability status of items', function () {
+
+      let elasticSearchResponse = require('./searchResponse.json')
+      let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
+      return availabilityResolver.responseWithUpdatedAvailability()
+        .then((newResponse) => {
+            return newResponse
+        })
+        .then((response) => {
+            var hit = response.hits.hits[0]
+            var items = hit._source.items
+            items.forEach((item) => {
+                if(item.status)
+                    console.log(item.uri + " : " + JSON.stringify(item.status))
+                if(item.uri === 'i10283665'){
+                    expect(item.status[0].id).to.equal('status:a')
+                    expect(item.status[0].label).to.equal('Available (For Unit Tests)')
+                }else if(item.uri === 'i10283664'){
+                    expect(item.status[0].id).to.equal('status:u')
+                    expect(item.status[0].label).to.equal('Temporarily unavailable')
+                }
+            })
+        })
+    })
+  })
