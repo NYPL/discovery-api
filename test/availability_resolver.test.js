5e3c730b499528b42feb75ca29e5e7ae3524cbfd
nodanaonlyzuul <beholdthepanda@gmail.com>
2017-06-16 16:57:41 -0400

    Implement more tests

--- test/availability_resolver.test.js
+++ test/availability_resolver.test.js
@@ -2,38 +2,76 @@
 
 let AvailabilityResolver = require('../lib/availability_resolver.js')
 let expect = require('chai').expect
+let elasticSearchResponse = require('./fixtures/elastic_search_response.json')
 
-function getFakeRestClient () {
+function getFakeRestClient (fakeResponse = null) {
+  if (!fakeResponse) {
+    // default fake response
+    var response = [
+      {
+        'itemBarcode': '33433058338470',
+        'itemAvailabilityStatus': "Item Barcode doesn't exist in SCSB database.",
+        'errorMessage': null
+      },
+      {
+        'itemBarcode': '32101071572406',
+        'itemAvailabilityStatus': 'Available',
+        'errorMessage': null
+      },
+      {
+        'itemBarcode': '1000546836',
+        'itemAvailabilityStatus': 'Not Available',
+        'errorMessage': null
+      }
+    ]
+  } else {
+    var response = fakeResponse
+  }
+
   return {
     getItemsAvailabilityForBarcodes: function (barcodes) {
-      return Promise.resolve([
-        {
-          'itemBarcode': '33433058338470',
-          'itemAvailabilityStatus': "Item Barcode doesn't exist in SCSB database.",
-          'errorMessage': null
-        },
-        {
-          'itemBarcode': '32101071572406',
-          'itemAvailabilityStatus': 'Available',
-          'errorMessage': null
-        },
-        {
-          'itemBarcode': '1000546836',
-          'itemAvailabilityStatus': 'Not Available',
-          'errorMessage': null
-        }
-      ])
+      return Promise.resolve(response)
     }
   }
 }
 
 describe('Response with updated availability', function () {
-  it('will change an items status to "Available" if ElasticSearch says it\'s unavailable but SCSB says it is Available')
+  it('will change an items status to "Available" if ElasticSearch says it\'s unavailable but SCSB says it is Available', function() {
+    let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
+
+    availabilityResolver.restClient = getFakeRestClient([{
+      'itemBarcode': '1000546836',
+      'itemAvailabilityStatus': 'Available',
+      'errorMessage': null}])
+
+    let indexedAsUnavailableURI = 'i10283664'
+
+    let indexedAsUnavailable = elasticSearchResponse.hits.hits[0]._source.items.find((item) => {
+      return item.uri === indexedAsUnavailableURI
+    })
+
+    // Test that it's unavailable at first
+    expect(indexedAsUnavailable.status[0].id).to.equal('status:u')
+    expect(indexedAsUnavailable.status[0].label).to.equal('Temporarily unavailable')
+
+    return availabilityResolver.responseWithUpdatedAvailability()
+    .then((modifedResponse) => {
+
+      let theItem = modifedResponse.hits.hits[0]._source.items.find((item) => {
+        return item.uri === indexedAsUnavailableURI
+      })
+
+      // Test AvailabilityResolver munges it into availability
+      expect(theItem.status[0].id).to.equal('status:a')
+      expect(theItem.status[0].label).to.equal('Available')
+    })
+
+  })
+
   it('will change an items status to "Unavailable" if ElasticSearch says it\'s Available but SCSB says it is Unvailable')
   it('will return the original ElasticSearch response if the response contains nothing with barcodes')
 
   it('includes the latest availability status of items', function () {
-    let elasticSearchResponse = require('./fixtures/elastic_search_response.json')
     let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
     availabilityResolver.restClient = getFakeRestClient()
 
