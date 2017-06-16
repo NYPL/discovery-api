7013f8a8b40ddeded7387a1a2e2657e37de91aeb
nodanaonlyzuul <beholdthepanda@gmail.com>
2017-06-16 12:59:12 -0400

    Adding pending (for now) specs

--- test/availability_resolver.test.js
+++ test/availability_resolver.test.js
@@ -4,6 +4,10 @@ var AvailabilityResolver = require('../lib/availability_resolver.js')
 var expect = require('chai').expect
 
 describe('Response with updated availability', function () {
+  it ('will change an items status to "Available" if ElasticSearch says it\'s unavailable but SCSB says it is Available')
+  it ('will change an items status to "Unavailable" if ElasticSearch says it\'s Available but SCSB says it is Unvailable')
+  it ('will return the original ElasticSearch response if the response contains nothing with barcodes')
+
   it('includes the latest availability status of items', function () {
     let elasticSearchResponse = require('./fixtures/elastic_search_response.json')
     let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
