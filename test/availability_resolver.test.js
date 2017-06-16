e453a5511e1914a628e9b69a9dae4f8c0ab3d0ee
nodanaonlyzuul <beholdthepanda@gmail.com>
2017-06-16 15:49:56 -0400

    stub out the SCSB client in a way that allows us to easily fake responses.

--- test/availability_resolver.test.js
+++ test/availability_resolver.test.js
@@ -1,9 +1,35 @@
 /* eslint-env mocha */
 
-var AvailabilityResolver = require('../lib/availability_resolver.js')
-var expect = require('chai').expect
+let AvailabilityResolver = require('../lib/availability_resolver.js')
+let expect = require('chai').expect
+let sinon  = require('sinon')
+
+function getFakeRestClient() {
+  return {
+      getItemsAvailabilityForBarcodes: function (barcodes) {
+        return Promise.resolve([
+  {
+    "itemBarcode": "33433058338470",
+    "itemAvailabilityStatus": "Item Barcode doesn't exist in SCSB database.",
+    "errorMessage": null
+  },
+  {
+    "itemBarcode": "32101071572406",
+    "itemAvailabilityStatus": "Available",
+    "errorMessage": null
+  },
+  {
+    "itemBarcode": "1000546836",
+    "itemAvailabilityStatus": "Not Available",
+    "errorMessage": null
+  }
+])
+      }
+    }
+}
 
 describe('Response with updated availability', function () {
+
   it ('will change an items status to "Available" if ElasticSearch says it\'s unavailable but SCSB says it is Available')
   it ('will change an items status to "Unavailable" if ElasticSearch says it\'s Available but SCSB says it is Unvailable')
   it ('will return the original ElasticSearch response if the response contains nothing with barcodes')
@@ -11,6 +37,8 @@ describe('Response with updated availability', function () {
   it('includes the latest availability status of items', function () {
     let elasticSearchResponse = require('./fixtures/elastic_search_response.json')
     let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
+    availabilityResolver.restClient = getFakeRestClient()
+
     return availabilityResolver.responseWithUpdatedAvailability()
         .then((newResponse) => {
           return newResponse
