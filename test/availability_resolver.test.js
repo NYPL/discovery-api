fc7969b6703ded25dcacc33f9f74319185e9a29f
Jobin Thomas <jobinthomas@nypl.org>
2017-06-16 12:40:25 -0400

    Ran standard js fix on availability resolver test file

--- test/availability_resolver.test.js
+++ test/availability_resolver.test.js
@@ -1,31 +1,29 @@
 /* eslint-env mocha */
 
 var AvailabilityResolver = require('../lib/availability_resolver.js')
-var expect = require("chai").expect;
+var expect = require('chai').expect
 
 describe('Response with updated availability', function () {
-    it('includes the latest availability status of items', function () {
-
-      let elasticSearchResponse = require('./searchResponse.json')
-      let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
-      return availabilityResolver.responseWithUpdatedAvailability()
+  it('includes the latest availability status of items', function () {
+    let elasticSearchResponse = require('./searchResponse.json')
+    let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
+    return availabilityResolver.responseWithUpdatedAvailability()
         .then((newResponse) => {
-            return newResponse
+          return newResponse
         })
         .then((response) => {
-            var hit = response.hits.hits[0]
-            var items = hit._source.items
-            items.forEach((item) => {
-                if(item.status)
-                    console.log(item.uri + " : " + JSON.stringify(item.status))
-                if(item.uri === 'i10283665'){
-                    expect(item.status[0].id).to.equal('status:a')
-                    expect(item.status[0].label).to.equal('Available')
-                }else if(item.uri === 'i10283664'){
-                    expect(item.status[0].id).to.equal('status:u')
-                    expect(item.status[0].label).to.equal('Temporarily unavailable')
-                }
-            })
+          var hit = response.hits.hits[0]
+          var items = hit._source.items
+          items.forEach((item) => {
+            if (item.status) { console.log(item.uri + ' : ' + JSON.stringify(item.status)) }
+            if (item.uri === 'i10283665') {
+              expect(item.status[0].id).to.equal('status:a')
+              expect(item.status[0].label).to.equal('Available')
+            } else if (item.uri === 'i10283664') {
+              expect(item.status[0].id).to.equal('status:u')
+              expect(item.status[0].label).to.equal('Temporarily unavailable')
+            }
+          })
         })
-    })
   })
+})
