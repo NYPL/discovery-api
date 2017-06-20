a5bcc399cbc3b64ebbde25567329c46cb7c30cbc
Jobin Thomas <jobinthomas@nypl.org>
2017-06-20 09:43:15 -0400

    Added pending unit tests for availability resolver. Reading fake json response from js file instead of json file

--- test/availability_resolver.test.js
+++ test/availability_resolver.test.js
@@ -2,31 +2,26 @@
 
 let AvailabilityResolver = require('../lib/availability_resolver.js')
 let expect = require('chai').expect
-let elasticSearchResponse = require('./fixtures/elastic_search_response.json')
+let elasticSearchResponse = require('./fixtures/elastic_search_response.js')
 
-function getFakeRestClient (fakeResponse = null) {
-  if (!fakeResponse) {
-    // default fake response
-    var response = [
-      {
-        'itemBarcode': '33433058338470',
-        'itemAvailabilityStatus': "Item Barcode doesn't exist in SCSB database.",
-        'errorMessage': null
-      },
-      {
-        'itemBarcode': '32101071572406',
-        'itemAvailabilityStatus': 'Available',
-        'errorMessage': null
-      },
-      {
-        'itemBarcode': '1000546836',
-        'itemAvailabilityStatus': 'Not Available',
-        'errorMessage': null
-      }
-    ]
-  } else {
-    var response = fakeResponse
-  }
+function getFakeRestClient () {
+  var response = [
+    {
+      'itemBarcode': '33433058338470',
+      'itemAvailabilityStatus': "Item Barcode doesn't exist in SCSB database.",
+      'errorMessage': null
+    },
+    {
+      'itemBarcode': '32101071572406',
+      'itemAvailabilityStatus': 'Not Available',
+      'errorMessage': null
+    },
+    {
+      'itemBarcode': '1000546836',
+      'itemAvailabilityStatus': 'Available',
+      'errorMessage': null
+    }
+  ]
 
   return {
     getItemsAvailabilityForBarcodes: function (barcodes) {
@@ -37,16 +32,13 @@ function getFakeRestClient (fakeResponse = null) {
 
 describe('Response with updated availability', function () {
   it('will change an items status to "Available" if ElasticSearch says it\'s unavailable but SCSB says it is Available', function () {
-    let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
+    let availabilityResolver = new AvailabilityResolver(elasticSearchResponse.fakeElasticSearchResponse())
 
-    availabilityResolver.restClient = getFakeRestClient([{
-      'itemBarcode': '1000546836',
-      'itemAvailabilityStatus': 'Available',
-      'errorMessage': null}])
+    availabilityResolver.restClient = getFakeRestClient()
 
     let indexedAsUnavailableURI = 'i10283664'
 
-    let indexedAsUnavailable = elasticSearchResponse.hits.hits[0]._source.items.find((item) => {
+    let indexedAsUnavailable = elasticSearchResponse.fakeElasticSearchResponse().hits.hits[0]._source.items.find((item) => {
       return item.uri === indexedAsUnavailableURI
     })
 
@@ -66,29 +58,76 @@ describe('Response with updated availability', function () {
     })
   })
 
-  it('will change an items status to "Unavailable" if ElasticSearch says it\'s Available but SCSB says it is Unvailable')
-  it('will return the original ElasticSearch response if the response contains nothing with barcodes')
+  it('will change an items status to "Unavailable" if ElasticSearch says it\'s Available but SCSB says it is Unvailable', function () {
+    let availabilityResolver = new AvailabilityResolver(elasticSearchResponse.fakeElasticSearchResponse())
+    availabilityResolver.restClient = getFakeRestClient()
+
+    let indexedAsAvailableURI = 'i10283665'
+    let indexedAsAvailable = elasticSearchResponse.fakeElasticSearchResponse().hits.hits[0]._source.items.find((item) => {
+      return item.uri === indexedAsAvailableURI
+    })
+
+    // Test that it's available at first
+    expect(indexedAsAvailable.status[0].id).to.equal('status:a')
+    expect(indexedAsAvailable.status[0].label).to.equal('Available')
+
+    return availabilityResolver.responseWithUpdatedAvailability()
+      .then((modifiedResponse) => {
+        let theItem = modifiedResponse.hits.hits[0]._source.items.find((item) => {
+          return item.uri === indexedAsAvailableURI
+        })
+
+        // Test AvailabilityResolver munges it into temporarily unavailable
+        expect(theItem.status[0].id).to.equal('status:u')
+        expect(theItem.status[0].label).to.equal('Temporarily unavailable')
+      })
+  })
+  it('will return the original ElasticSearchResponse\'s status for the item if the SCSB can\'t find an item with the barcode', function () {
+    let availabilityResolver = new AvailabilityResolver(elasticSearchResponse.fakeElasticSearchResponse())
+    availabilityResolver.restClient = getFakeRestClient()
+
+    let indexedButNotAvailableInSCSBURI = 'i22566485'
+    let indexedButNotAvailableInSCSB = elasticSearchResponse.fakeElasticSearchResponse().hits.hits[0]._source.items.find((item) => {
+      return item.uri === indexedButNotAvailableInSCSBURI
+    })
+
+    expect(indexedButNotAvailableInSCSB.status[0].id).to.equal('status:a')
+    expect(indexedButNotAvailableInSCSB.status[0].label).to.equal('Available')
+
+    return availabilityResolver.responseWithUpdatedAvailability()
+      .then((modifiedResponse) => {
+        let theItem = modifiedResponse.hits.hits[0]._source.items.find((item) => {
+          return item.uri === indexedButNotAvailableInSCSBURI
+        })
+
+        // As this item is not available in SCSB, the elasticSearchResponse's availability for the item was returned
+        expect(theItem.status[0].id).to.equal('status:a')
+        expect(theItem.status[0].label).to.equal('Available')
+      })
+  })
 
   it('includes the latest availability status of items', function () {
-    let availabilityResolver = new AvailabilityResolver(elasticSearchResponse)
+    let availabilityResolver = new AvailabilityResolver(elasticSearchResponse.fakeElasticSearchResponse())
     availabilityResolver.restClient = getFakeRestClient()
 
     return availabilityResolver.responseWithUpdatedAvailability()
-        .then((newResponse) => {
-          return newResponse
+        .then((modifedResponse) => {
+          return modifedResponse
         })
         .then((response) => {
-          var hit = response.hits.hits[0]
-          var items = hit._source.items
-          items.forEach((item) => {
-            if (item.uri === 'i10283665') {
-              expect(item.status[0].id).to.equal('status:a')
-              expect(item.status[0].label).to.equal('Available')
-            } else if (item.uri === 'i10283664') {
-              expect(item.status[0].id).to.equal('status:u')
-              expect(item.status[0].label).to.equal('Temporarily unavailable')
-            }
+          var items = response.hits.hits[0]._source.items
+
+          var unavailableItem = items.find((item) => {
+            return item.uri === 'i10283665'
           })
+          expect(unavailableItem.status[0].id).to.equal('status:u')
+          expect(unavailableItem.status[0].label).to.equal('Temporarily unavailable')
+
+          var availableItem = items.find((item) => {
+            return item.uri === 'i10283664'
+          })
+          expect(availableItem.status[0].id).to.equal('status:a')
+          expect(availableItem.status[0].label).to.equal('Available')
         })
   })
 })
