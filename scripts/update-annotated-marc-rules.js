2500d9885793dbe7713119ee04031836b10a13ac
Paul Beaudoin <contact@paulbeaudoin.com>
2018-03-08 15:19:04 -0500

    Add bib-record-index-rules logic to annotated-marc
    Adds parsing of bib-record-index-rules, the result of which is used as a
    secondary set of marc tag and indicator patterns that must be matched
    before employing a given label. Includes full unit testing.

--- scripts/update-annotated-marc-rules.js
+++ scripts/update-annotated-marc-rules.js
@@ -8,13 +8,16 @@ const AnnotatedMarcSerializer = require('../lib/annotated-marc-serializer')
 
 // Read raw webpub.def
 const mappingRulesRaw = fs.readFileSync('./data/webpub.def', 'utf8')
+// Read raw bib-record-index-rules.txt
+const bibRecordIndexRules = fs.readFileSync('./data/bib-record-index-rules.txt', 'utf8')
 
 // Transform raw webpub.def into a series of mapping rules:
-const mappingRules = AnnotatedMarcSerializer.parseWebpubToAnnotatedMarcRules(mappingRulesRaw)
+const mappingRules = AnnotatedMarcSerializer.buildAnnotatedMarcRules(mappingRulesRaw, bibRecordIndexRules)
   .map((rule) => {
     return Object.assign({}, rule, {
       // RegExp.proto.source returns .toString() without '/' bookends
-      marcIndicatorRegExp: rule.marcIndicatorRegExp.source
+      marcIndicatorRegExp: rule.marcIndicatorRegExp.source,
+      secondaryMarcIndicatorRegExp: rule.secondaryMarcIndicatorRegExp.source
     })
   })
 
