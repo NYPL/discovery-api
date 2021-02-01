64f157120b7d6a02c22319063c0eb97021df992f
Daniel Appel <dan.s.appel@gmail.com>
2021-02-01 12:19:56 -0500

    Remove console log

--- routes/resources.js
+++ routes/resources.js
@@ -88,7 +88,6 @@ module.exports = function (app) {
   app.get(`/api/v${VER}/discovery/resources/:uri\.:ext?`, function (req, res) {
     var gatheredParams = gatherParams(req, ['uri', 'items_size', 'items_from'])
     var params = { uri: req.params.uri }
-    console.log('items_size type', typeof gatheredParams.items_size)
     if (Number.isInteger(parseInt(gatheredParams.items_size))) params.items_size = gatheredParams.items_size
     if (Number.isInteger(parseInt(gatheredParams.items_from))) params.items_from = gatheredParams.items_from
 
