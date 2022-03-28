a600baa028906c3a65f4ab184be65704f0ed37a7
Vera Kahn <verakahn@nypl.org>
2022-03-28 12:37:04 -0400

    tests pass

---
+++ test/parallel-fields-extractor.test.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai')
+const parallelFieldsExtractor = require('../lib/parallel-fields-extractor')
+const parallelFieldsBib = require('./fixtures/parallel-fields-response.json')
+const elasticSearchResponseFixture = require('./fixtures/query-023c19c59c3511325e7d0e3046fcc8b1.json')
+
+describe.only('Parallel Fields Extractor', () => {
+  describe('When a bib has a parallel fields property', () => {
+    it('returns the elasticSearchResponse object', () => {
+      const response = parallelFieldsExtractor(parallelFieldsBib)
+      expect(response.hits.hits).to.be.an('array')
+    })
+    it('adds each of the items in that array to the bibs as parallel<FieldName>', () => {
+      const parallelsExtracted = parallelFieldsExtractor(parallelFieldsBib).hits.hits[0]._source
+      expect(Object.keys(parallelsExtracted).length).to.equal(4)
+      expect(parallelsExtracted).to.deep.equal({ 'parallelPublicationStatement': ['Москва : Вагриус, 2006.'], 'parallelTableOfContents': ['Черный маг -- Копыто инженера -- Вечер страшной субботы -- Великий канцлер -- Фантастический роман -- Золотое Копье -- Князь тьмы -- Мастер и Маргарита (полная рукописная редакция) -- Мастер и Маргарита (окончательная редакция).'], 'parallelNote': ['\"Литературно-художественное издание\"--Colophon.'], 'parallelPlaceOfPublication': ['Москва :'] })
+    })
+  })
+  describe('When a bib has no parallel fields property', () => {
+    it('does not modify the elasticSearchResponse', () => {
+      expect(parallelFieldsExtractor(elasticSearchResponseFixture)).to.equal(elasticSearchResponseFixture)
+    })
+  })
+})
