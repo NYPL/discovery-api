7df4541240f90a633fb6484e25f03c9d37294af6
Vera Kahn <verakahn@nypl.org>
2022-05-30 21:56:24 -0400

    change tests to check for params

--- test/resources_routes.test.js
+++ test/resources_routes.test.js
@@ -3,33 +3,37 @@ const { expect } = require('chai')
 const sinon = require('sinon')
 const app = require('../app')
 
-describe.only('resources routes', function () {
+describe('resources routes', function () {
   describe('item id', function () {
     it('recognizes harvard item ids', function () {
       const findByUriSpy = sinon.spy(app.resources.findByUri)
       axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/hb990000049180203941-hi231730706390003941`).then(() => {
-        expect(findByUriSpy.callCount).to.equal(1)
+        const params = {uri: 'hb990000049180203941', itemUri: 'hi231730706390003941'}
+        sinon.assert.calledWith(findByUriSpy, params)
       }
       )
     })
     it('recognizes pi item ids', function () {
       const findByUriSpy = sinon.spy(app.resources.findByUri)
       axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/pb990000049180203941-pi231730706390003941`).then(() => {
-        expect(findByUriSpy.callCount).to.equal(1)
+        const params = {uri: 'pb990000049180203941', itemUri: 'pi231730706390003941'}
+        sinon.assert.calledWith(findByUriSpy, params)
       }
       )
     })
     it('recognizes ci item ids', function () {
       const findByUriSpy = sinon.spy(app.resources.findByUri)
       axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/cb9900000-ci2317307`).then(() => {
-        expect(findByUriSpy.callCount).to.equal(1)
+        const params = {uri: 'cb9900000', itemUri: 'ci2317307'}
+        sinon.assert.calledWith(findByUriSpy, params)
       }
       )
     })
     it('recognizes i item ids', function () {
       const findByUriSpy = sinon.spy(app.resources.findByUri)
       axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/b12345678-i23173070`).then(() => {
-        expect(findByUriSpy.callCount).to.equal(1)
+        const params = {uri: 'b12345678', itemUri: 'i23173070'}
+        sinon.assert.calledWith(findByUriSpy, params)
       }
       )
     })
