3721dca8952fefeef085cdf24026514201a06bfd
Vera Kahn <verakahn@nypl.org>
2022-06-07 09:42:34 -0400

    WIP

--- test/resources_routes.test.js
+++ test/resources_routes.test.js
@@ -3,14 +3,19 @@ const { expect } = require('chai')
 const sinon = require('sinon')
 const fixtures = require('./fixtures')
 
-describe('resources routes', function () {
+describe.only('resources routes', function () {
   let app
-
+  let paramsToTest
   before(function () {
     fixtures.enableEsFixtures()
     fixtures.enableScsbFixtures()
 
     app = require('../app')
+
+    sinon.stub(app.resources, 'findByUri').callsFake((params) => {
+      paramsToTest = params
+      return { response: 'response' }
+    })
   })
 
   after(function () {
@@ -20,43 +25,41 @@ describe('resources routes', function () {
 
   describe('item id', function () {
     it('recognizes harvard item ids', function () {
-      const findByUriSpy = sinon.spy(app.resources.findByUri)
-      axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/hb990000049180203941-hi231730706390003941`).then(() => {
-        const params = {uri: 'hb990000049180203941', itemUri: 'hi231730706390003941'}
-        sinon.assert.calledWith(findByUriSpy, params)
+      return axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/hb990000049180203941-hi231730706390003941`).then(() => {
+        expect(paramsToTest).to.equal({ uri: 'hb990000049180203941', itemUri: 'hi231730706390003941' })
       }
       )
     })
-    it('recognizes pi item ids', function () {
-      const findByUriSpy = sinon.spy(app.resources.findByUri)
-      axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/pb990000049180203941-pi231730706390003941`).then(() => {
-        const params = {uri: 'pb990000049180203941', itemUri: 'pi231730706390003941'}
-        sinon.assert.calledWith(findByUriSpy, params)
-      }
-      )
-    })
-    it('recognizes ci item ids', function () {
-      const findByUriSpy = sinon.spy(app.resources.findByUri)
-      axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/cb9900000-ci2317307`).then(() => {
-        const params = {uri: 'cb9900000', itemUri: 'ci2317307'}
-        sinon.assert.calledWith(findByUriSpy, params)
-      }
-      )
-    })
-    it('recognizes i item ids', function () {
-      const findByUriSpy = sinon.spy(app.resources.findByUri)
-      axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/b12345678-i23173070`).then(() => {
-        const params = {uri: 'b12345678', itemUri: 'i23173070'}
-        sinon.assert.calledWith(findByUriSpy, params)
-      }
-      )
-    })
-    it('rejects non item ids', function () {
-      const findByUriSpy = sinon.spy(app.resources.findByUri)
-      axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/pb9900000-xx2317307`).then(() => {
-        expect(findByUriSpy.callCount).to.equal(0)
-      }
-      )
-    })
+    // it('recognizes pi item ids', function () {
+    //   const findByUriSpy = sinon.spy(app.resources.findByUri)
+    //   axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/pb990000049180203941-pi231730706390003941`).then(() => {
+    //     const params = {uri: 'pb990000049180203941', itemUri: 'pi231730706390003941'}
+    //     sinon.assert.calledWith(findByUriSpy, params)
+    //   }
+    //   )
+    // })
+    // it('recognizes ci item ids', function () {
+    //   const findByUriSpy = sinon.spy(app.resources.findByUri)
+    //   axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/cb9900000-ci2317307`).then(() => {
+    //     const params = {uri: 'cb9900000', itemUri: 'ci2317307'}
+    //     sinon.assert.calledWith(findByUriSpy, params)
+    //   }
+    //   )
+    // })
+    // it('recognizes i item ids', function () {
+    //   const findByUriSpy = sinon.spy(app.resources.findByUri)
+    //   axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/b12345678-i23173070`).then(() => {
+    //     const params = {uri: 'b12345678', itemUri: 'i23173070'}
+    //     sinon.assert.calledWith(findByUriSpy, params)
+    //   }
+    //   )
+    // })
+    // it('rejects non item ids', function () {
+    //   const findByUriSpy = sinon.spy(app.resources.findByUri)
+    //   axios.get(`${global.TEST_BASE_URL}/api/v0.1/discovery/resources/pb9900000-xx2317307`).then(() => {
+    //     expect(findByUriSpy.callCount).to.equal(0)
+    //   }
+    //   )
+    // })
   })
 })
