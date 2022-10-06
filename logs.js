c6fa2628fc8b74e7be7f828715392d76255d74a9
Vera Kahn <verakahn@nypl.org>
2022-10-06 11:39:51 -0400

    writes events to file

--- logs.js
+++ logs.js
@@ -1,4 +1,5 @@
-const { CloudWatchLogsClient, StartQueryCommand, GetQueryResultsCommand } = require("@aws-sdk/client-cloudwatch-logs"); // CommonJS import
+const { CloudWatchLogsClient, StartQueryCommand, GetQueryResultsCommand } = require("@aws-sdk/client-cloudwatch-logs")
+const { writeFileSync } = require('fs')
 
 const client = new CloudWatchLogsClient({ accessKeyId: 'AKIA5YTHPEF4S7ZQNPX7', secretAccessKey: '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', region: 'us-east-1' });
 
@@ -7,19 +8,50 @@ start.setMonth(start.getMonth() - 3)
 const end = Date.now()
 
 const inputs = []
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
   start.setDate(start.getDate() + 2)
   inputs.push({
     "endTime": end,
     "logGroupName": "/aws/elasticbeanstalk/discovery-api-production/var/log/nginx/access.log",
-    "queryString": "fields @message | sort @timestamp desc | filter @message like \"/resources?\" | filter @message not like \"filters\" | limit 10 | filter message not like \"oclc\"",
+    "queryString": "fields @message | sort @timestamp desc | filter @message like \"/resources?\" | filter @message not like \"filters\" | limit 10000 | filter message not like \"oclc\"",
     "startTime": Date.parse(start)
   })
 }
 
 const delay = async (time) => new Promise((resolve) => setTimeout(resolve, time))
 
-const theThing = async () => {
+const checkResultStatusAndDownload = async (queryIds, completedQueries = {}, numberOfChecks = 1) => {
+  while (queryIds.length !== Object.keys(completedQueries).length) {
+    console.log('starting check ' + numberOfChecks)
+    await Promise.all(queryIds.map(async (queryId, i) => {
+      // Ensure there are less than 5 requests per second to avoid throttling
+      await delay(220)
+      if (!completedQueries[queryId]) {
+        const queryResultsCommand = new GetQueryResultsCommand({ queryId })
+        try {
+          const { status, results } = await client.send(queryResultsCommand)
+          if (status === 'Complete') {
+            console.log(`query at index ${i} complete`)
+            const queryResults = results.map(result => {
+              const value = result[0].value.split(' ')
+              return value[3] + value[5]
+            }).join('\t')
+            writeFileSync(`log-${queryId.split('-')[0]}.json`, queryResults)
+            completedQueries[queryId] = true
+          }
+        } catch (e) {
+          if (e.__type === "ThrottlingException") console.log('throttle exception')
+          else console.log(e)
+        }
+      }
+    }))
+    await delay(10000)
+    return await checkResultStatusAndDownload(queryIds, completedQueries, ++numberOfChecks)
+  }
+  return true
+}
+
+const queryAndWriteDiscoveryApiLogs = async () => {
   const queryIds = await Promise.all(inputs.map(async (input) => {
     const startQueryCommand = new StartQueryCommand(input)
     let queryId
@@ -28,26 +60,11 @@ const theThing = async () => {
     } catch (e) {
       console.log(e)
     }
-    await delay(1000)
     return queryId
   }))
-  await delay(300000)
-  console.log('middle delay')
 
-  const queryResults = await Promise.all(queryIds.map(async (queryId) => {
-    const queryResultsCommand = new GetQueryResultsCommand({ queryId })
-    let queryResult
-    try {
-      ({ results } = await client.send(queryResultsCommand))
-      queryResult = results
-    } catch (e) {
-      console.log(e)
-    }
-    await delay(1000)
-    return queryResult
-  }))
-  console.log(queryResults)
+  await checkResultStatusAndDownload(queryIds)
 }
 
-theThing()
+queryAndWriteDiscoveryApiLogs()
 
