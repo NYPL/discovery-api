a5e6160169755f5991d556c02d1191ee2dfc3b1e
Paul Beaudoin <contact@paulbeaudoin.com>
2024-10-31 10:08:16 -0400

    Fix small bug with identifying nypl checkin items
    Fix small bug where items generated from holdings record checkin cards failed
    to be identiifed as NYPL items. This had no known impact on behavior, but
    betrayed the "isItemNyplOwned" function's promise

--- test/ownership_determination.test.js
+++ test/ownership_determination.test.js
@@ -2,7 +2,12 @@ const isItemNyplOwned = require('../lib/ownership_determination').isItemNyplOwne
 
 describe('isItemNyplOwned', function () {
   it('will only return true for certain items', function () {
-    const fakeNyplItems = [{ uri: 'i10022734' }, { uri: 'i123489' }]
+    const fakeNyplItems = [
+      { uri: 'i10022734' },
+      { uri: 'i123489' },
+      // An item generated from a checkin card in a holding has this form:
+      { uri: 'i-h1234-1' }
+    ]
     const fakeColumbiaItems = [{ uri: 'ci10022734' }, { uri: 'ci123489' }]
     const fakePrincetonItems = [{ uri: 'pi10022734' }, { uri: 'pi123489' }]
     const fakeNonsenseItems = [{ uri: 'jsaoisjosjiaosjio' }, { uri: 'hello-mother' }]
