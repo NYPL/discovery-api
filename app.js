01917a1e771c8abcde64c7c364ad3f750557938a
Vera Kahn <verakahn@nypl.org>
2025-01-15 15:53:46 -0500

    Revert "rm console.log"
    This reverts commit c36744321e0471e82d07baac53b49e4db68f7e6a.

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const app = express()
 app.set('trust proxy', 'loopback')
 
 app.init = async () => {
+  console.log('heeeeere')
   await loadConfig.loadConfig()
   await loadNyplCoreData()
   preflightCheck()
