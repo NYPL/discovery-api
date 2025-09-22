a781232948ee1c3ced0540d30a02e4103a61221f
Vera Kahn <verakahn@nypl.org>
2025-09-22 16:34:09 -0400

    fix es doc property

--- test/resources-responses.test.js
+++ test/resources-responses.test.js
@@ -267,7 +267,7 @@ describe('Test Resources responses', function () {
         assert(doc.itemAggregations)
 
         assert.deepEqual(doc.format, [{ '@id': 'a', prefLabel: 'Book/Text' }])
-        //
+
         // assert.deepEqual(doc.collection, [])
 
         done()
