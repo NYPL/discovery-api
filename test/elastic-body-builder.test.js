1c769c9cddef35a85f02e8ac48f3658677eac8d5
danamansana <danielappel@nypl.org>
2026-01-16 15:36:19 -0500

    Add bodybuilder tests

--- test/elastic-body-builder.test.js
+++ test/elastic-body-builder.test.js
@@ -140,6 +140,237 @@ describe('bodyForSearch', function () {
   })
 })
 
-describe('bodyForFindByUri', async function () {
-  bodyForFindByUri()
+describe('bodyForFindByUri', function () {
+  it('queries for uri', function () {
+    const expected = {
+      _source: {
+        excludes: [
+          'uris',
+          '*_packed',
+          '*_sort',
+          'items.*_packed',
+          'contentsTitle',
+          'suppressed',
+          '*WithoutDates',
+          '*Normalized',
+          'items'
+        ]
+      },
+      size: 1,
+      query: {
+        bool: {
+          must: [
+            {
+              term: {
+                uri: 'b15781267'
+              }
+            }
+          ],
+          filter: [
+            {
+              bool: {
+                should: [
+                  {
+                    nested: {
+                      path: 'items',
+                      query: {
+                        bool: {
+                          must: {
+                            match_all: {}
+                          }
+                        }
+                      },
+                      inner_hits: {
+                        sort: [
+                          {
+                            'items.enumerationChronology_sort': 'desc'
+                          }
+                        ],
+                        size: 100,
+                        from: 0,
+                        name: 'items'
+                      }
+                    }
+                  },
+                  {
+                    match_all: {}
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      },
+      aggregations: {
+        item_location: {
+          nested: {
+            path: 'items'
+          },
+          aggs: {
+            _nested: {
+              terms: {
+                size: 100,
+                field: 'items.holdingLocation_packed'
+              }
+            }
+          }
+        },
+        item_status: {
+          nested: {
+            path: 'items'
+          },
+          aggs: {
+            _nested: {
+              terms: {
+                size: 100,
+                field: 'items.status_packed'
+              }
+            }
+          }
+        },
+        item_format: {
+          nested: {
+            path: 'items'
+          },
+          aggs: {
+            _nested: {
+              terms: {
+                size: 100,
+                field: 'items.formatLiteral'
+              }
+            }
+          }
+        }
+      }
+    }
+
+    const params = {
+      all_items: false,
+      uri: 'b15781267',
+      items_size: 100,
+      items_from: 0,
+      merge_checkin_card_items: true,
+      include_item_aggregations: true
+    }
+    const barcodes = {}
+    expect(bodyForFindByUri(barcodes, params))
+      .to.deep.equal(expected)
+  })
+
+  it('accepts item params', function () {
+    const barcodes = { 'Not Available': ['1234'] }
+    const params = {
+      all_items: false,
+      uri: 'b15781267',
+      items_size: 10,
+      items_from: 10,
+      merge_checkin_card_items: true,
+      include_item_aggregations: true
+    }
+
+    const expected = {
+      _source: {
+        excludes: [
+          'uris',
+          '*_packed',
+          '*_sort',
+          'items.*_packed',
+          'contentsTitle',
+          'suppressed',
+          '*WithoutDates',
+          '*Normalized',
+          'items'
+        ]
+      },
+      size: 1,
+      query: {
+        bool: {
+          must: [
+            {
+              term: {
+                uri: 'b15781267'
+              }
+            }
+          ],
+          filter: [
+            {
+              bool: {
+                should: [
+                  {
+                    nested: {
+                      path: 'items',
+                      query: {
+                        bool: {
+                          must: {
+                            match_all: {}
+                          }
+                        }
+                      },
+                      inner_hits: {
+                        sort: [
+                          {
+                            'items.enumerationChronology_sort': 'desc'
+                          }
+                        ],
+                        size: 10,
+                        from: 10,
+                        name: 'items'
+                      }
+                    }
+                  },
+                  {
+                    match_all: {}
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      },
+      aggregations: {
+        item_location: {
+          nested: {
+            path: 'items'
+          },
+          aggs: {
+            _nested: {
+              terms: {
+                size: 100,
+                field: 'items.holdingLocation_packed'
+              }
+            }
+          }
+        },
+        item_status: {
+          nested: {
+            path: 'items'
+          },
+          aggs: {
+            _nested: {
+              terms: {
+                size: 100,
+                field: 'items.status_packed'
+              }
+            }
+          }
+        },
+        item_format: {
+          nested: {
+            path: 'items'
+          },
+          aggs: {
+            _nested: {
+              terms: {
+                size: 100,
+                field: 'items.formatLiteral'
+              }
+            }
+          }
+        }
+      }
+    }
+
+    expect(bodyForFindByUri(barcodes, params))
+      .to.deep.equal(expected)
+  })
 })
