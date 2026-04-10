a19d88f36031f445a56944afc0a71cfdfdbf70ef
Vera Kahn <verakahn@nypl.org>
2026-04-10 09:27:10 -0400

    ensure int test failure

--- test/integration/delivery-locations-by-barcode.test.js
+++ test/integration/delivery-locations-by-barcode.test.js
@@ -46,19 +46,20 @@ const getDeliveryLocations = async (barcode, patronId) => {
 }
 
 const theThing = async () => {
-  await loadConfig()
-  const results = await Promise.all(Object.keys(ptypes).map((checkLocationsForPtype)))
-  const resultsHaveProblems = Object.keys(ptypes).some((ptype, i) => {
-    const resultsForPtype = results[i]
-    if (resultsForPtype.problems.length) {
-      console.error(`Error with ${ptype} ptype delivery results, `, resultsForPtype.problems)
-      return true
-    } else {
-      console.log(`All delivery location checks for ${ptype} patron type successful`)
-      return false
-    }
-  })
-  if (resultsHaveProblems) throw new Error('Delivery location checks failed.')
+  throw new Error('spaghetti')
+  // await loadConfig()
+  // const results = await Promise.all(Object.keys(ptypes).map((checkLocationsForPtype)))
+  // const resultsHaveProblems = Object.keys(ptypes).some((ptype, i) => {
+  //   const resultsForPtype = results[i]
+  //   if (resultsForPtype.problems.length) {
+  //     console.error(`Error with ${ptype} ptype delivery results, `, resultsForPtype.problems)
+  //     return true
+  //   } else {
+  //     console.log(`All delivery location checks for ${ptype} patron type successful`)
+  //     return false
+  //   }
+  // })
+  // if (resultsHaveProblems) throw new Error('Delivery location checks failed.')
 }
 
 theThing()
