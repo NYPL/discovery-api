f756dd831f7028098706be4fe320a47653eccb90
Chris Mulholland <cmulholland@gmail.com>
2026-06-27 18:41:19 -0400

    Style: standardize nyql helpers formatting

--- test/integration/nyql/helpers.js
+++ test/integration/nyql/helpers.js
@@ -1,9 +1,7 @@
-// this file to contain constants and helper functions for the integration tests
-
 const baseUrl = process.env.NYQL_TEST_BASE_URL || 'https://qa-platform.nypl.org/api/v0.1'
 const getId = (item) => item?.result?.['@id']
 
 module.exports = {
-	baseUrl,
-	getId,
-};
+  baseUrl,
+  getId
+}
