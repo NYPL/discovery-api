186d934fce4e32d503e2f297492023ec97565fa0
sarangj <sarangjoshi22@gmail.com>
2026-08-04 09:50:15 -0400

    Wrap server in newrelic agent
    Add a simple shell script to conditionally check for the existence of
    the necessary NEW_RELIC_LICENSE_KEY environment var, to conditionally run the
    server as is, or wrapped in the newrelic agent.

---
+++ start-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ -z "${NEW_RELIC_LICENSE_KEY}" ]]; then
+  node server.js
+else
+  if [[ -n "${LOCAL}" ]]; then
+    export NEW_RELIC_APP_NAME="Discovery API (local)"
+  else
+    export NEW_RELIC_APP_NAME="Discovery API (${ENV})"
+  fi
+
+  node -r newrelic server.js
+fi
