6dc3174dc82adb686433c1028ab5a7e5cfb6fb57
Vera Kahn <verakahn@nypl.org>
2026-08-20 13:21:21 -0400

    linting

--- routes/resources.js
+++ routes/resources.js
@@ -1,114 +1,114 @@
-const VER = "0.1";
-const logger = require("../lib/logger");
+const VER = '0.1'
+const logger = require('../lib/logger')
 
 module.exports = function (app) {
-  app.all("*", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    app.baseUrl = `http${req.secure ? "s" : ""}://${req.get("host")}/api/v${VER}/discovery`;
-    const requestLogObject = {ip: req.ip, path: req.path, body: req.body, query: req.query, method: req.method}
+  app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    app.baseUrl = `http${req.secure ? 's' : ''}://${req.get('host')}/api/v${VER}/discovery`
+    const requestLogObject = { ip: req.ip, path: req.path, body: req.body, query: req.query, method: req.method }
     logger.info(`${req.method} request to ${req.path}:`, requestLogObject)
-    next();
-  });
+    next()
+  })
 
   const respond = (res, _resp, params) => {
-    let contentType = "application/ld+json";
-    if (params.ext === "ntriples") contentType = "text/plain";
+    let contentType = 'application/ld+json'
+    if (params.ext === 'ntriples') contentType = 'text/plain'
 
-    let resp = _resp;
-    if (contentType !== "text/plain") resp = JSON.stringify(_resp, null, 2);
+    let resp = _resp
+    if (contentType !== 'text/plain') resp = JSON.stringify(_resp, null, 2)
 
-    res.type(contentType);
-    res.status(200).send(resp);
-    return true;
-  };
+    res.type(contentType)
+    res.status(200).send(resp)
+    return true
+  }
 
   app.get(`/api/v${VER}/discovery/resources$`, function (req, res, next) {
-    const params = req.query;
+    const params = req.query
 
-    const hasFilters = params.filters && Object.keys(params.filters).length > 0;
+    const hasFilters = params.filters && Object.keys(params.filters).length > 0
 
-    if (params.include_aggregations === "true" && !hasFilters) {
+    if (params.include_aggregations === 'true' && !hasFilters) {
       return app.resources
         .search(
           params,
           { baseUrl: app.baseUrl, include_aggregations: true },
-          req,
+          req
         )
         .then((resp) => respond(res, resp, params))
-        .catch((error) => next(error));
-    } else if (params.include_aggregations === "true") {
+        .catch((error) => next(error))
+    } else if (params.include_aggregations === 'true') {
       return Promise.all([
         app.resources.search(params, { baseUrl: app.baseUrl }, req),
-        app.resources.aggregations(params, { baseUrl: app.baseUrl }),
+        app.resources.aggregations(params, { baseUrl: app.baseUrl })
       ])
         .then(([searchResp, aggsResp]) => {
-          searchResp.aggregations = aggsResp.itemListElement;
-          return respond(res, searchResp, params);
+          searchResp.aggregations = aggsResp.itemListElement
+          return respond(res, searchResp, params)
         })
-        .catch((error) => next(error));
+        .catch((error) => next(error))
     } else {
       return app.resources
         .search(params, { baseUrl: app.baseUrl }, req)
         .then((resp) => respond(res, resp, params))
-        .catch((error) => next(error));
+        .catch((error) => next(error))
     }
-  });
+  })
 
   app.get(
     `/api/v${VER}/discovery/resources/aggregations`,
     function (req, res, next) {
-      const params = req.query;
+      const params = req.query
 
       return app.resources
         .aggregations(params, { baseUrl: app.baseUrl })
         .then((resp) => respond(res, resp, params))
-        .catch((error) => next(error));
-    },
-  );
+        .catch((error) => next(error))
+    }
+  )
 
   app.get(
     `/api/v${VER}/discovery/resources/aggregations/:field`,
     function (req, res, next) {
-      const params = Object.assign({}, req.query, req.params);
+      const params = Object.assign({}, req.query, req.params)
 
       return app.resources
         .aggregation(params, { baseUrl: app.baseUrl })
         .then((resp) => respond(res, resp, params))
-        .catch((error) => next(error));
-    },
-  );
+        .catch((error) => next(error))
+    }
+  )
 
   app.get(`/api/v${VER}/discovery/browse/subjects`, function (req, res, next) {
-    const params = req.query;
+    const params = req.query
 
     return app.subjects
       .browse(params, { baseUrl: app.baseUrl }, req)
       .then((resp) => respond(res, resp, params))
-      .catch((error) => next(error));
-  });
+      .catch((error) => next(error))
+  })
 
   app.get(
     `/api/v${VER}/discovery/browse/contributors`,
     function (req, res, next) {
-      const params = req.query;
+      const params = req.query
 
       return app.contributors
         .browse(params, { baseUrl: app.baseUrl }, req)
         .then((resp) => respond(res, resp, params))
-        .catch((error) => next(error));
-    },
-  );
+        .catch((error) => next(error))
+    }
+  )
 
   app.get(`/api/v${VER}/discovery/vocabularies`, function (req, res, next) {
-    const params = Object.assign({}, req.query, req.params);
+    const params = Object.assign({}, req.query, req.params)
 
     return app
       .vocabularies(params, { baseUrl: app.baseUrl })
       .then((resp) => respond(res, resp, params))
-      .catch((error) => next(error));
-  });
+      .catch((error) => next(error))
+  })
 
   /*
    * Return items with `deliveryLocation`s matching the supplied barcodes
@@ -119,15 +119,15 @@ module.exports = function (app) {
   app.get(
     `/api/v${VER}/request/deliveryLocationsByBarcode`,
     function (req, res, next) {
-      const params = req.query;
+      const params = req.query
 
-      const handler = app.resources.deliveryLocationsByBarcode;
+      const handler = app.resources.deliveryLocationsByBarcode
 
       return handler(params, { baseUrl: app.baseUrl })
         .then((resp) => respond(res, resp, params))
-        .catch((error) => next(error));
-    },
-  );
+        .catch((error) => next(error))
+    }
+  )
 
   /**
    *  Item route
@@ -138,14 +138,14 @@ module.exports = function (app) {
   app.get(
     `/api/v${VER}/discovery/resources/:uri-:itemUri([a-z]?i[a-z0-9]+)`,
     function (req, res, next) {
-      const params = { uri: req.params.uri, itemUri: req.params.itemUri };
+      const params = { uri: req.params.uri, itemUri: req.params.itemUri }
 
       return app.resources
         .findByUri(params, { baseUrl: app.baseUrl }, req)
         .then((responseBody) => respond(res, responseBody, params))
-        .catch((error) => next(error));
-    },
-  );
+        .catch((error) => next(error))
+    }
+  )
 
   /**
    * Bib route
@@ -156,24 +156,22 @@ module.exports = function (app) {
   app.get(
     `/api/v${VER}/discovery/resources/:uri.:ext?`,
     function (req, res, next) {
-      const params = Object.assign({}, req.query, { uri: req.params.uri });
+      const params = Object.assign({}, req.query, { uri: req.params.uri })
 
-      if (Number.isInteger(parseInt(req.query.items_size)))
-        params.items_size = req.query.items_size;
-      if (Number.isInteger(parseInt(req.query.items_from)))
-        params.items_from = req.query.items_from;
+      if (Number.isInteger(parseInt(req.query.items_size))) { params.items_size = req.query.items_size }
+      if (Number.isInteger(parseInt(req.query.items_from))) { params.items_from = req.query.items_from }
 
-      let handler = app.resources.findByUri;
+      let handler = app.resources.findByUri
 
-      if (req.params.ext === "annotated-marc") {
-        handler = app.resources.annotatedMarc;
-      } else if (req.params.ext === "marc") {
-        handler = app.resources.marc;
+      if (req.params.ext === 'annotated-marc') {
+        handler = app.resources.annotatedMarc
+      } else if (req.params.ext === 'marc') {
+        handler = app.resources.marc
       }
 
       return handler(params, { baseUrl: app.baseUrl }, req)
         .then((responseBody) => respond(res, responseBody, params))
-        .catch((error) => next(error));
-    },
-  );
-};
+        .catch((error) => next(error))
+    }
+  )
+}
