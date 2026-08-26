695c36b28e1dca67bee7f9fede627e231649f32d
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-08-26 10:58:58 -0400

    start of new display situation

--- test/display-field-unpacker.test.js
+++ test/display-field-unpacker.test.js
@@ -2,19 +2,19 @@ const { expect } = require('chai')
 const displayFieldsUnpacker = require('../lib/display-field-unpacker')
 const packedDisplayBib = require('./fixtures/packed-display-response.json')
 
-describe('Display field unpacker', () => {
-  describe('When a bib has a packed display property', () => {
-    it('adds each of the items in that array as unpacked objects', () => {
+describe('Display field parser', () => {
+  describe('When a bib has a display components property', () => {
+    it('adds each of the items in that array as a name, label, nameTitle object', () => {
       const displayFieldsUnpacked = displayFieldsUnpacker(packedDisplayBib).hits.hits[0]._source
       expect(Object.keys(displayFieldsUnpacked).length).to.equal(2)
       expect(displayFieldsUnpacked).to.deep.equal({
         testDisplay: [
-          { value: 'someValue', display: 'someDisplay' },
-          { value: 'someValueB', display: 'someDisplayB' },
-          { value: 'someValueC', display: null }
+          { label: 'Smith, John, author', name: 'Smith, John', nameTitle: 'Smith, John' },
+          { label: 'Bayer, Jeffrey. Cataloging test record.', name: 'Bayer, Jeffrey', nameTitle: 'Bayer, Jeffrey. Cataloging test record' },
+          { label: 'Org Inc.', name: 'Org Inc', nameTitle: 'Org Inc' }
         ],
         testOtherDisplay: [
-          { value: 'otherValue', display: 'otherDisplay' }
+          { label: 'Bean, Richard, 1956-, editor', name: 'Bean, Richard, 1956-', nameTitle: 'Bean, Richard, 1956-' }
         ]
       })
     })
